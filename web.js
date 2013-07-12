1dbeff3e6d0900758a7a0a539fff5119e0507d77
Ubuntu <ubuntu@ip-172-31-40-245.us-west-2.compute.internal>
2013-07-12 19:25:30 +0000

    read from file try 4

--- web.js
+++ web.js
@@ -10,23 +10,21 @@ app.get('/', function(request, response) {
   //  fs.readFile ('/index.html', function (err. data) {
 //	if (err) throw err;
 //	response.send(data);
-
-fs.exists (fileName, function (exists) {
-    if (exists) {
-	fs.stat (fileName, function (error, stats) {
-	    fs.open(fileName, "r", function (error, fd) {
-		var buffer = new Buffer (stats.size);
-		fs.read (fd, buffer, 0, buffer.length, null, function (error, bytesRead, buffer) {
-		    var data = buffer.toString("utf8", 0, buffer.length);
-
-
-		    response.send (data);
-		    fs.close(fd);
+    fs.exists(fileName, function(exists) {
+	if (exists) {
+	    fs.stat(fileName, function(error, stats) {
+		fs.open(fileName, "r", function(error, fd) {
+		    var buffer = new Buffer(stats.size);
+		    fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+			var data = buffer.toString("utf8", 0, buffer.length);
+			response.send(data);
+			fs.close(fd);
 		    });
 		});
 	    });
-	});
-    }
+	}
+    });
+	
 });
 
 var port = process.env.PORT || 5000;
