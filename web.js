c9fe4f7fe8f2d0611f07cf4fec20c5bef7ef3df8
Ubuntu <ubuntu@ip-172-31-40-245.us-west-2.compute.internal>
2013-07-12 18:51:35 +0000

    read from file 2

--- web.js
+++ web.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var fs = require ("fs");
+var fileName = "/index.html";
+
 
 var app = express.createServer(express.logger());
 
@@ -7,7 +10,22 @@ app.get('/', function(request, response) {
   //  fs.readFile ('/index.html', function (err. data) {
 //	if (err) throw err;
 //	response.send(data);
-    response.send (fs.readFile('/index.html');
+
+fs.exists (fileName, function (exists) {
+    if (exists) {
+	fs.stat (fileName, function (error, stats) {
+	    fs.open(fileName, "r", function (error, fd) {
+		var buffer = new Buffer (stats.size);
+		fs.read (fd, buffer, 0, buffer.length, null, function (error, bytesRead, buffer) {
+		    var data = buffer.toString("utf8", 0, buffer.length);
+
+
+		    response.send (data);
+		    fs.close(fd);
+		    });
+		});
+	    });
+	});
     }
 });
 
